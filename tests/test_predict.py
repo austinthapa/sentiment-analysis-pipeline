bfdcf3bb3220f1845a59e94239a9f6498599c54d
Austin Thapa <anilthapa1.at23@gmail.com>
2025-11-12 19:01:08 -0600

    feat: Add test for root routes

---
+++ tests/test_predict.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "Okay"}
+    
+def test_predict():
+    response = client.post("/predict", json = {"text": "I love this product"})
+    assert response.status_code == 200
+    assert "prediction" in response.json()
